405f27da9209e6a0c554b06ff46a4c57a5d5fe4e
zhangchuanpan <1099311614@qq.com>
2025-09-30 18:06:33 +0800

    集成supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
+
 function App() {
+  const [accounts, setAccounts] = useState([]);
+
+  async function getAccounts() {
+    console.log("Fetching accounts from Supabase...");
+    const { data, error } = await supabase.from("Acount").select();
+    console.log("Supabase response:", { data, error });
+    console.log("__________")
+    if (error) {
+        console.error("Both table names failed");
+        setAccounts([]);
+    } else {
+      setAccounts(data || []);
+    }
+  }
+
+  useEffect(() => { getAccounts(); }, []);
+
   return (
-    <>
-      <h1>Welcome To My Test Web!!!</h1>
-    </>
-  )
+      <ul>
+        {accounts.map((item) => (
+          <li key={item.name}>{item.name}</li>
+        ))}
+      </ul>
+  );
 }
 
 export default App
